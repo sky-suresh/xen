d87c7a812361e34a4bb942ed9698c2ebbbd189d7
ramesh <sureshkumarr2710@gmail.com>
2026-01-16 10:11:13 +0000

    changes123

--- provider.tf
+++ provider.tf
@@ -8,9 +8,8 @@ terraform {
 }
 
 provider "azurerm" {
- features {
-   
- }
+ features {}
 
   subscription_id = "252026ba-6735-4965-9bbf-6bad9c701532"
+  tenant_id = "ddba09d2-5005-40ad-b8b5-6f7967a19bcf"
 }
